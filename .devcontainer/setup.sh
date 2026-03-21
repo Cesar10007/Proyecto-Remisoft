5011b7e0d79178f498c3435fd492ffe64e83165d
César David Rueda Daza <163583831+Cesar10007@users.noreply.github.com>
2026-03-21 01:07:15 +0000

    Cambio de imagen php8

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 echo "Configurando entorno RemiSoft..."
 
-# Agregar repositorio sury y instalar driver pgsql
-curl -sSLo /tmp/php.gpg https://packages.sury.org/php/apt.gpg
-cp /tmp/php.gpg /usr/share/keyrings/php.gpg
-echo "deb [signed-by=/usr/share/keyrings/php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list
+# Instalar PostgreSQL
 apt-get update -y 2>/dev/null || true
-apt-get install -y postgresql postgresql-contrib php8.2-pgsql || true
+apt-get install -y postgresql postgresql-client || true
 
 # Iniciar PostgreSQL
-pg_ctlcluster 13 main start || true
+service postgresql start || true
 sleep 3
 
 # Crear usuario y base de datos
-psql -U postgres -c "DO \$\$ BEGIN
+su -s /bin/bash postgres -c "psql -c \"DO \\\$\\\$ BEGIN
   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'remisoft') THEN
     CREATE USER remisoft WITH PASSWORD 'remisoft123';
   END IF;
-END \$\$;"
+END \\\$\\\$;\""
 
-psql -U postgres -c "SELECT 'CREATE DATABASE remisoft OWNER remisoft' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'remisoft')\gexec"
+su -s /bin/bash postgres -c "psql -c \"SELECT 'CREATE DATABASE remisoft OWNER remisoft' WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'remisoft')\gexec\""
 
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE remisoft TO remisoft;"
+su -s /bin/bash postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE remisoft TO remisoft;\""
 
-psql -U postgres -d remisoft -f /workspaces/Proyecto-Remisoft/DBFAMILIAREMI.sql
+su -s /bin/bash postgres -c "psql -d remisoft -f /workspaces/Proyecto-Remisoft/DBFAMILIAREMI.sql"
+
+# Instalar driver pgsql para PHP desde pecl
+apt-get install -y libpq-dev php-dev || true
+pecl install pdo_pgsql || true
+echo "extension=pdo_pgsql.so" >> /usr/local/etc/php/conf.d/pdo_pgsql.ini
 
 echo "Base de datos lista."
