2b9de089d6e23a736e9817c5fc104ef31802b36c
César David Rueda Daza <163583831+Cesar10007@users.noreply.github.com>
2026-04-30 22:17:25 +0000

    Eliminar carpeta ejercicio

--- ejercicio/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="app-footer">
-      <p>Prototipo de interfaz de proyecto formativo con componentes reutilizables.</p>
-    </footer>
-  )
-}
