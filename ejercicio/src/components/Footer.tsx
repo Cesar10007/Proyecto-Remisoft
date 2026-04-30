7217b78d1d09ba091dea9111765ad9ca56906892
César David Rueda Daza <163583831+Cesar10007@users.noreply.github.com>
2026-04-30 22:03:58 +0000

    Ejercicio fredy

---
+++ ejercicio/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="app-footer">
+      <p>Prototipo de interfaz de proyecto formativo con componentes reutilizables.</p>
+    </footer>
+  )
+}
