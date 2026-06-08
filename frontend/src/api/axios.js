5ecc08c8821fa563859286db99f0db1f247660a3
César David Rueda Daza <163583831+Cesar10007@users.noreply.github.com>
2026-06-08 22:18:13 +0000

    feat: implementar recuperación de contraseña con Mailtrap

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://ominous-space-system-pj7w9xq4q6g5hr677-8000.app.github.dev/api',
   headers: {
     'Content-Type': 'application/json',
     Accept: 'application/json',
